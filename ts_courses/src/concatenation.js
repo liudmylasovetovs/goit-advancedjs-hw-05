67a4136009b64a528743c77a842d1a072d8485a0
Liudmyla Sovetovs <milkaegik@gmail.com>
2024-08-25 23:08:27 -0700

    dgadfg

--- ts_courses/src/concatenation.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.concatenation = concatenation;
-function concatenation(firstWord, secondWord) {
-    console.log("".concat(firstWord, " ").concat(secondWord));
-}
