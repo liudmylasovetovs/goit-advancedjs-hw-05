bf6954525e25c54576d249d31682f37de3b1769c
Liudmyla Sovetovs <milkaegik@gmail.com>
2024-08-25 23:03:58 -0700

    fin

---
+++ ts_courses/src/concatenation.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.concatenation = concatenation;
+function concatenation(firstWord, secondWord) {
+    console.log("".concat(firstWord, " ").concat(secondWord));
+}
